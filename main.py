cc625f1e6b6dbe5684028cc8d1e7712c7980fc5d
Stasyaosan <smavrin38@gmail.com>
2024-03-31 17:10:50 +0300

    First_Comit

---
+++ main.py
@@ -0,0 +1,58 @@
+import json
+import time
+
+import requests
+
+
+class Text2ImageAPI:
+
+    def __init__(self, url, api_key, secret_key):
+        self.URL = url
+        self.AUTH_HEADERS = {
+            'X-Key': f'Key {api_key}',
+            'X-Secret': f'Secret {secret_key}',
+        }
+
+    def get_model(self):
+        response = requests.get(self.URL + 'key/api/v1/models', headers=self.AUTH_HEADERS)
+        data = response.json()
+        return data[0]['id']
+
+    def generate(self, prompt, model, images=1, width=1024, height=1024):
+        params = {
+            "type": "GENERATE",
+            "numImages": images,
+            "width": width,
+            "height": height,
+            "generateParams": {
+                "query": f"{prompt}"
+            }
+        }
+
+        data = {
+            'model_id': (None, model),
+            'params': (None, json.dumps(params), 'application/json')
+        }
+        response = requests.post(self.URL + 'key/api/v1/text2image/run', headers=self.AUTH_HEADERS, files=data)
+        data = response.json()
+        return data['uuid']
+
+    def check_generation(self, request_id, attempts=10, delay=10):
+        while attempts > 0:
+            response = requests.get(self.URL + 'key/api/v1/text2image/status/' + request_id, headers=self.AUTH_HEADERS)
+            data = response.json()
+            if data['status'] == 'DONE':
+                return data['images']
+
+            attempts -= 1
+            time.sleep(delay)
+
+
+if __name__ == '__main__':
+    api = Text2ImageAPI('https://api-key.fusionbrain.ai/', '1B40ABA68882A545B7409EA760EF2473', '3BB36A223968D69203EBFBE0B6EF3083')
+    model_id = api.get_model()
+    uuid = api.generate("Sun in sky", model_id)
+    images = api.check_generation(uuid)
+    print(images)
+
+#Не забудьте указать именно ваш YOUR_KEY и YOUR_SECRET.
